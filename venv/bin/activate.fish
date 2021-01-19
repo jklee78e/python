571ddfb25ca4dfabf6a369a0ea96d41568cb61d3
JaeKyung LEE <jklee78e@gmail.com>
2021-01-19 14:49:48 +0900

    lab

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kiie/python/venv"
+set -gx VIRTUAL_ENV "/home/jklee/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
